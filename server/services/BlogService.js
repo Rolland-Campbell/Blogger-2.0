4bd737aae7c36e7d2ec4734a3e292c7b616f76bd
Rolland <Rollandcampbell@msn.com>
2019-09-08 14:41:19 -0600

    init

---
+++ server/services/BlogService.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const _model = new Schema({
+  title: { type: String, max: 60, required: true },
+  summary: { type: String, max: 120 },
+  img: { type: String },
+  body: { type: String },
+  author: { type: ObjectId, ref: 'User', required: true }
+}, { timestamps: true })
+
+export default class BlogService {
+  get repository() {
+    return mongoose.model('Blog', _model)
+  }
+}
